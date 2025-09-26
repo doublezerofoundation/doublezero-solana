d5b369a992d3ec1d100c16e7653e97d6f446ffb9
Karl <214665535+karl-dz@users.noreply.github.com>
2025-09-26 13:48:09 -0500

    client-tools: add tracing feature

---
+++ crates/client-tools/src/log.rs
@@ -0,0 +1,57 @@
+#[macro_export]
+macro_rules! log_info {
+    ($($arg:tt)*) => {
+        #[cfg(feature = "tracing")]
+        {
+            ::tracing::info!($($arg)*);
+        }
+        #[cfg(not(feature = "tracing"))]
+        {
+            println!($($arg)*);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! log_error {
+    ($($arg:tt)*) => {
+        #[cfg(feature = "tracing")]
+        {
+            ::tracing::error!($($arg)*);
+        }
+        #[cfg(not(feature = "tracing"))]
+        {
+            eprintln!($($arg)*);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! log_warn {
+    ($($arg:tt)*) => {
+        #[cfg(feature = "tracing")]
+        {
+            ::tracing::warn!($($arg)*);
+        }
+        #[cfg(not(feature = "tracing"))]
+        {
+            eprintln!($($arg)*);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! log_debug {
+    ($($arg:tt)*) => {
+        #[cfg(feature = "tracing")]
+        {
+            ::tracing::debug!($($arg)*);
+        }
+        #[cfg(not(feature = "tracing"))]
+        {
+            // Debug messages are typically not shown in release builds
+            #[cfg(debug_assertions)]
+            println!($($arg)*);
+        }
+    };
+}
