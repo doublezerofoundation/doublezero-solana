03f26ea43ad1a89e0e86b64853e008a36bf91c20
Karl <214665535+karl-dz@users.noreply.github.com>
2025-11-25 12:27:41 -0600

    add new state vars

--- programs/revenue-distribution/src/state/distribution.rs
+++ programs/revenue-distribution/src/state/distribution.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 use bytemuck::{Pod, Zeroable};
 use doublezero_program_tools::{
     types::{Flags, StorageGap},
@@ -85,7 +87,11 @@ pub struct Distribution {
     pub distributed_2z_amount: u64,
     pub burned_2z_amount: u64,
 
-    _storage_gap: StorageGap<7>,
+    pub processed_solana_validator_debt_write_off_start_index: u32,
+    pub processed_solana_validator_debt_write_off_end_index: u32,
+    _padding_1: [u8; 24],
+
+    _storage_gap: StorageGap<6>,
 }
 
 impl PrecomputedDiscriminator for Distribution {
@@ -185,6 +191,23 @@ impl Distribution {
         } else {
             Some(i64::from(allowed_timestamp))
         }
+    }
+
+    #[inline]
+    pub fn processed_solana_validator_debt_bitmap_range(&self) -> Range<usize> {
+        self.processed_solana_validator_debt_start_index as usize
+            ..self.processed_solana_validator_debt_end_index as usize
+    }
+
+    #[inline]
+    pub fn processed_rewards_bitmap_range(&self) -> Range<usize> {
+        self.processed_rewards_start_index as usize..self.processed_rewards_end_index as usize
+    }
+
+    #[inline]
+    pub fn processed_solana_validator_debt_write_off_bitmap_range(&self) -> Range<usize> {
+        self.processed_solana_validator_debt_write_off_start_index as usize
+            ..self.processed_solana_validator_debt_write_off_end_index as usize
     }
 }
 
